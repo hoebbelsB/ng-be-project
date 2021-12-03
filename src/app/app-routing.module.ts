05ba03bba0630e62f33fa6932e8805ee367fef89
Julian Jandl <julian@jandl.wien>
2021-12-03 12:44:55 +0100

    init

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { AboutModule } from './about/about.module';
 
 const routes: Routes = []
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes), AboutModule],
+  imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
